623ad5cb883087642aa5f089c0925b8499d587b4
hidems <miura.eleven@gmail.com>
2022-11-30 20:36:21 +0100

    Vuex Actions

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,28 +1,40 @@
 <template>
   <div>
     <h1>Produvt List</h1>
-    <ul>
+    <img 
+      v-if="loading"
+      src="https://i.imgur.com/JfPpwOA.gif"
+    />
+    <ul v-else>
       <li v-for="product in products">{{product.title}}-{{product.price}}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import shop from '@/api/shop'
 import store from '@/store/index'
 
 export default {
+  data () {
+    return {
+      loading: false
+    }
+  },
+
   computed: {
-    products() {
+    products () {
       // return store.state.products
       return store.getters.availableProducts
     }
   },
   
   created () {
-    shop.getProducts(products => {
-      store.commit('setProducts', products)
-    })
+    this.loading = true
+    store.dispatch('fetchProducts')
+      .then(() => this.loading = false)
+    // shop.getProducts(products => {
+    //   store.commit('setProducts', products)
+    // })
   },
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
+import shop from '@/api/shop'
 
 Vue.use(Vuex)
 
@@ -14,9 +15,16 @@ export default new Vuex.Store({
     }
   },
 
-  actions: {
-    fetchProducts() {
-
+  actions: { // = methods
+    fetchProducts({ commit }) {
+      return new Promise((resolve, reject) => {
+        // make the call
+        // call setProducts mutation
+        shop.getProducts(products => {
+          commit('setProducts', products)
+          resolve()
+        })
+      })
     }
   },
 
