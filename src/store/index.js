8e4440b6a05d5d5bd892983fde6b8aedb1853698
hidems <miura.eleven@gmail.com>
2022-12-10 14:17:01 +0100

    Checkout

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,7 @@ export default new Vuex.Store({
     products: [],
     // id, quantity
     cart: [],
+    checkoutStatus: null,
   },
 
   getters: { // = computed properties
@@ -55,6 +56,19 @@ export default new Vuex.Store({
         context.commit('decrementProductInventory', product)
       }
     },
+
+    checkout({ state, commit }) {
+      shop.buyProducts(
+        state.cart,
+        () => {
+          commit('emptyCart')
+          commit('setCheckoutStatus', 'success')
+        },
+        () => {
+          commit('setCheckoutStatus', 'fail')
+        },
+      )
+    },
   },
 
   mutations: {
@@ -77,5 +91,13 @@ export default new Vuex.Store({
     decrementProductInventory(state, product) {
       product.inventory--
     },
+
+    setCheckoutStatus(state, status) {
+      state.checkoutStatus = status
+    },
+
+    emptyCart(state) {
+      state.cart = []
+    }
   },
 })
